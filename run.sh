0f93822c7f06b1e4100a833ffb77ef59b64470b7
Arnaud Bienvenu <abi@abienvenu.net>
2018-05-08 19:09:34 +0200

    Fix image name in run.sh script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run --rm -it -v $HOME:$HOME -e DISPLAY=$DISPLAY -e USER -e USERID=$(id -u) -v /tmp/.X11-unix:/tmp/.X11-unix josm/josm
+docker run --rm -it -v $HOME:$HOME -e DISPLAY=$DISPLAY -e USER -e USERID=$(id -u) -v /tmp/.X11-unix:/tmp/.X11-unix abienvenu/josm
 
